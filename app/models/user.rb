93f6b261dbaa7ca3a418003bd20120b3b1db8119
Samuel-Quenouille <quenouille_samuel@yahoo.com>
2023-09-06 18:28:51 +0200

    maj

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
-	# Il faut ajouter les deux modules commençant par jwt
-	devise :database_authenticatable, :registerable,
-	:jwt_authenticatable,
-	jwt_revocation_strategy: JwtDenylist
+    has_many :properties
+
+    validates :email, 
+    presence: true,
+    uniqueness: true,
+    format: { with: /\A[^@\s]+@([^@\s]+.)+[^@\s]+\z/, message: "Entrez un email valide" }
+
+    devise :database_authenticatable, :registerable,
+    :jwt_authenticatable,
+    jwt_revocation_strategy: JwtDenylist
 end
