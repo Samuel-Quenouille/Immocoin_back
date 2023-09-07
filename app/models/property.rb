b74d3548550a990a6c4cebfe0a36be0ca75c6f4f
Samuel-Quenouille <quenouille_samuel@yahoo.com>
2023-09-07 12:53:15 +0200

    maj validates

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,6 +1,2 @@
 class Property < ApplicationRecord
-    belongs_to :user
-
-    validates :title, :description, :price, :city, :zip_code, presence: true
-    validates :price, numericality: { greater_than_or_equal_to: 0 }
 end
